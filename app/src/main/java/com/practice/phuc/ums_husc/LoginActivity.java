f6e993a9aa8968f64e97b716f188ad8c42578aac
Dai Phuc Tran <trandaiphuc.1501@gmail.com>
2019-03-14 20:41:39 +0700

    Initial commit

---
+++ app/src/main/java/com/practice/phuc/ums_husc/LoginActivity.java
@@ -0,0 +1,256 @@
+package com.practice.phuc.ums_husc;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Debug;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends AppCompatActivity {
+    /**
+     * Keep track of the login task to ensure we can cancel it if requested.
+     */
+    private UserLoginTask mAuthTask = null;
+
+    // UI references.
+    private AutoCompleteTextView txtMaSinhVien;
+    private EditText txtMatKhau;
+    private ViewGroup mProgressViewLayout;
+    private View mProgressView;
+    private View mLoginFormView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        // Set up the login form.
+        txtMaSinhVien = (AutoCompleteTextView) findViewById(R.id.txt_ma_sinh_vien);
+        txtMatKhau = (EditText) findViewById(R.id.txt_mat_khau);
+        txtMatKhau.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        Button btnLogin = (Button) findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+
+        mProgressViewLayout = findViewById(R.id.login_progress_layout);
+        mLoginFormView = findViewById(R.id.login_form);
+        mProgressView = findViewById(R.id.login_progress);
+    }
+
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private void attemptLogin() {
+        if (mAuthTask != null) {
+            return;
+        }
+
+        // Reset errors.
+        txtMaSinhVien.setError(null);
+        txtMatKhau.setError(null);
+
+        // Store values at the time of the login attempt.
+        String maSinhVien = txtMaSinhVien.getText().toString();
+        String matKhau = txtMatKhau.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Validate ma sinh vien
+        if (TextUtils.isEmpty(maSinhVien)) {
+            txtMaSinhVien.setError(getString(R.string.error_truong_bat_buoc));
+            focusView = txtMaSinhVien;
+            cancel = true;
+        } else if (!isMaSinhVienValid(maSinhVien)) {
+            txtMaSinhVien.setError(getString(R.string.error_ma_sinh_vien_khong_hop_le));
+            focusView = txtMaSinhVien;
+            cancel = true;
+        }
+        // Validate mat khau
+        else if (TextUtils.isEmpty(matKhau)) {
+            txtMatKhau.setError(getString(R.string.error_truong_bat_buoc));
+            focusView = txtMatKhau;
+            cancel = true;
+        } else if (!isPasswordValid(matKhau)) {
+            txtMatKhau.setError(getString(R.string.error_mat_khau_qua_ngan));
+            focusView = txtMatKhau;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            showProgress(true);
+            mAuthTask = new UserLoginTask(maSinhVien, matKhau);
+            mAuthTask.execute((String) null);
+        }
+    }
+
+    private boolean isMaSinhVienValid(String maSinhVien) {
+        return maSinhVien.contains("T");
+    }
+
+    private boolean isPasswordValid(String password) {
+        return password.length() >= 6;
+    }
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mProgressViewLayout.setVisibility(show ? View.GONE : View.VISIBLE);
+            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressViewLayout.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressViewLayout.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressViewLayout.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressViewLayout.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+
+    /**
+     * Thuc hien kiem tra thong tin dang nhap
+     */
+    public class UserLoginTask extends AsyncTask<String, Void, Boolean> {
+
+        private final String mMaSinhVien;
+        private final String mMatKhau;
+
+        UserLoginTask(String email, String password) {
+            mMaSinhVien = email;
+            mMatKhau = password;
+        }
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            // TODO: attempt authentication against a network service.
+            PostData(params);
+            try {
+                // Simulate network access.
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                return false;
+            }
+
+            // TODO: register the new account here.
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            mAuthTask = null;
+            showProgress(false);
+
+            if (success) {
+                Toast.makeText(LoginActivity.this, "Login success !!!", Toast.LENGTH_SHORT).show();
+            } else {
+                txtMatKhau.setError(getString(R.string.error_mat_khau_khong_dung));
+                txtMatKhau.requestFocus();
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            mAuthTask = null;
+            showProgress(false);
+        }
+
+        public String PostData(String[] valuse) {
+            String s = "";
+            OkHttpClient okHttpClient = new OkHttpClient();
+            RequestBody formBody = new FormBody.Builder()
+//                    .add("maSinhVien", valuse[0])
+//                    .add("matKhau", valuse[1])
+//                    .add("search", "Jurassic Park")
+                    .build();
+            Request request = new Request.Builder()
+//                    .url("")
+                    .url("http://192.168.1.106:56353/api/Values")
+                    .build();
+            okHttpClient.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    Log.d("UMS_HUSC", "Lỗi !!!" + e.getMessage());
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    Log.d("UMS_HUSC", response.body().string());
+                }
+            });
+            return s;
+        }
+    }
+}
+
